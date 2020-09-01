a0fc782fd4af5f3d7094878c0d34c534077fc086
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-09-02 01:38:27 +1000

    touch ups 2

---
+++ build/precache-manifest.4e87d10de414cdd58a83d28e8561f3ac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb8c527a3df3195e045c1f0dfecc8a5",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "fdf6147ca2c50ebc278a",
+    "url": "/keeper-app/static/js/2.b16fc963.chunk.js"
+  },
+  {
+    "revision": "4281f3eabee47483f495",
+    "url": "/keeper-app/static/js/main.6092e169.chunk.js"
+  },
+  {
+    "revision": "ed9243ad83c12b8fd98c",
+    "url": "/keeper-app/static/js/runtime-main.604284a8.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8071e706fc86bd3f7e269546eedae20b.js"
+  "/keeper-app/precache-manifest.4e87d10de414cdd58a83d28e8561f3ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/keeper-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
