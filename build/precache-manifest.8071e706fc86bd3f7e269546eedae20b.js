04ba2f461e740dbfbc591e94e1499419d7bb54c2
kookie09 <62861248+kookie09@users.noreply.github.com>
2020-09-02 01:29:21 +1000

    touch ups 2

---
+++ build/precache-manifest.8071e706fc86bd3f7e269546eedae20b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ae9922654648c104c7173606c3880e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdf6147ca2c50ebc278a",
+    "url": "/static/js/2.b16fc963.chunk.js"
+  },
+  {
+    "revision": "4281f3eabee47483f495",
+    "url": "/static/js/main.6092e169.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
